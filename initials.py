3f86f79311e16228da810db7b0543e69b612ea10
Kelvin Obeng Nyarko <2425403537@live.gctu.edu.gh>
2026-05-31 11:45:53 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,1 +1,32 @@
 
+# File: KON.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Kelvin Obeng Nyarko 
+# STUDENT ID: 2425403537
+# Email: 2425403537@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Kelvin Obeng Nyarko, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+print()
+
+print("...KON")
+
+print()
+
+print("...KKKK....KKKK..........OOOOOOOO..........NNNN....NNNN.....")
+print("...KKKK...KKKK..........OOOOOOOOOO.........NNNNN...NNNN.....")
+print("...KKKK..KKKK..........OOOO....OOOO........NNNNN...NNNN.....")
+print("...KKKK.KKK............OOOO....OOOO........NNNNNN..NNNN.....")
+print("...KKKK.KKK............OOOO....OOOO........NNNNNNN.NNNN.....")
+print("...KKKK.KKKK...........OOOO....OOOO........NNNN.NNNNNNN.....")
+print("...KKKK..KKKK..........OOOO....OOOO........NNNN..NNNNNN.....")
+print("...KKKK...KKKK.........OOOO....OOOO........NNNN...NNNNN.....")
+print("...KKKK....KKKK...**....OOOOOOOOOO....**...NNNN...NNNNN...**")
+print("...KKKK....KKKK...**.....OOOOOOOO.....**...NNNN....NNNN...**")
+
+
+print()
